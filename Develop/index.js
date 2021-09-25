d177899390d765d3ac9eb57a9e827c789ead5c4e
davidsdottir <aileen.bastos@gmail.com>
2021-09-25 11:35:42 -0400

    added packages for application

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 // TODO: Create an array of questions for user input
 const questions = [];
