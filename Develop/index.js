7f46d7c1a5da9408a4eb8f8bace1064690d957b3
davidsdottir <aileen.bastos@gmail.com>
2021-09-25 17:06:03 -0400

    add some sections

--- Develop/index.js
+++ Develop/index.js
@@ -4,8 +4,60 @@ const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
 // TODO: Create an array of sections for user input
-
-
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project? (Required)',
+        validate: projectInput => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log('Please enter the title of your project!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project. (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Provide installation instructions. (Required)',
+        validate: installInput => {
+          if (installInput) {
+            return true;
+          } else {
+            console.log('You need to enter installation instructions!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage instructions. (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('You need to enter usage instructions!');
+            return false;
+          }
+        }
+      },
 // TODO: Create an array of questions for user input
 const questions = [
     {
