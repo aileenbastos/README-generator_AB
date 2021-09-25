f6754acb088fea95a01aa122765500cbadeee48f
davidsdottir <aileen.bastos@gmail.com>
2021-09-25 14:18:38 -0400

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,8 +3,38 @@ const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
+// TODO: Create an array of sections for user input
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address!');
+            return false;
+          }
+        }
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
